05c69b439b3eb44d6f819fafb5acf9fa27d99bf4
mcBorgar <borgarfb@gmail.com>
2025-01-09 12:53:08 +0100

    html

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql2');
 
+// Oppsett for å håndtere JSON
+app.use(express.json());
+
+// En test-rute
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Serveren fungerer!');
+});
 
+// Start serveren
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-skibidi
+  console.log(`Server kjører på http://localhost:${port}`);
+});
+
+// Opprett en kobling til databasen
+const db = mysql.createConnection({
+  host: 'localhost',   // Serveradresse (lokalt)
+  user: 'root',        // Brukernavn til databasen
+  password: '',        // Passord til databasen (tom hvis ingen passord)
+  database: 'test_api' // Navnet på databasen vi skal bruke
+});
+
+// Test tilkobling
+db.connect((err) => {
+  if (err) {
+    console.error('Feil ved tilkobling til databasen:', err);
+  } else {
+    console.log('Koblet til MySQL-databasen!');
+  }
+});
