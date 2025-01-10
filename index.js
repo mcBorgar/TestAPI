1c9ed621292766170c4f5f1e9a88922e8df498b0
mcBorgar <borgarfb@gmail.com>
2025-01-10 11:05:46 +0100

    save, vs code krasja

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mysql = require('mysql2');
 
-// Oppsett for å håndtere JSON
+// Middleware for å håndtere JSON
 app.use(express.json());
 
-// En test-rute
+// Testendepunkt for å sjekke om API-et fungerer
 app.get('/', (req, res) => {
-  res.send('Serveren fungerer!');
+  res.send('API fungerer!');
 });
 
 // Start serveren
 app.listen(port, () => {
   console.log(`Server kjører på http://localhost:${port}`);
-});
-
-// Opprett en kobling til databasen
-const db = mysql.createConnection({
-  host: 'localhost',   // Serveradresse (lokalt)
-  user: 'root',        // Brukernavn til databasen
-  password: '',        // Passord til databasen (tom hvis ingen passord)
-  database: 'test_api' // Navnet på databasen vi skal bruke
-});
-
-// Test tilkobling
-db.connect((err) => {
-  if (err) {
-    console.error('Feil ved tilkobling til databasen:', err);
-  } else {
-    console.log('Koblet til MySQL-databasen!');
-  }
 });
